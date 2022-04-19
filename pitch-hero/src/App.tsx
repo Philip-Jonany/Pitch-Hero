5e7f22dcd104c309776890170ed7e93cd025d4d0
Philip-Jonany <72227955+Philip-Jonany@users.noreply.github.com>
2022-04-19 14:22:04 -0700

    literally copy pasted from JS, full of bugfs

--- pitch-hero/src/App.tsx
+++ pitch-hero/src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 
-function App() {
+const BIBBY_SIZE = 20;
+const GAME_WIDTH = 500;
+const GAME_HEIGHT = 500;
+const GRAVITY = 6;
+
+function App() { 
+  const [bibbyPosition, setBibbyPosition] = useState(250);
+
+  useEffect(() => {
+    let timeID;
+    if (birdPosition < GAME_HEIGHT - BIRD_SIZE) {
+      timeID = setInterval(() => {
+        setBibbyPosition(birdPosition => birdPosition + GRAVITY);
+      }, 24);
+    }
+    return () => {
+      clearInterval(timeId);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <GameBox height ={GAME_HEIGHT} width = {GAME_WIDTH}>
+        <Bibby size = {BIBBY_SIZE} top = {bibbyPosition} />
+      </GameBox>
+    </Div>
   );
 }
 
 export default App;
+
+const Bibby = styled.div`
+  position:absolute;
+  background-color: red;
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
+  top:  ${(props) => props.size}px;
+  border-radius = 50%;
+`;
+
+const Div = styled.div`
+  display:flex;
+  width:100%;
+  justify-content:center;
+`
+
+const GameBox = styled.div`
+  height:  ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  background-color: deepskyblue;
+`
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;

3b895eaf3a4b1c22dd898cf3d35ea793d5cdf8d3
Philip-Jonany <72227955+Philip-Jonany@users.noreply.github.com>
2022-05-03 02:41:05 -0700

    automated testing with jest, easy to add new tests from now on

---
+++ pitch-hero/src/__tests__/basic-test.js
@@ -0,0 +1,11 @@
+// import sum from './sum';
+
+// it('sums numbers', () => {
+//   expect(sum(1, 2)).toEqual(3);
+//   expect(sum(2, 2)).toEqual(4);
+// });
+
+it('is true true?', () => {
+    expect(true);
+    expect(true);
+  });
